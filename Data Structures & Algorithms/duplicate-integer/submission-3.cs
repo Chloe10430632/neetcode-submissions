782401b3c4eb20ed551097949d35bd638b47a67c
Chloe10430632 <111947982+Chloe10430632@users.noreply.github.com>
2026-04-20 11:51:21 +0800

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        int count = 0;
+        foreach (int i in nums) {
+            for (int j = 0; j < nums.Length; j++) {
+                if (nums[j] == i) {
+                    count++;
+                }
+            }
+        }
+
+        if (count >= nums.Length+1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
